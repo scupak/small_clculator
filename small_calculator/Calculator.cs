d6d166bda02b9b3151d613b629648f085ba6fb10
scupak <anto1095@easv365.dk>
2020-09-10 09:36:24 +0200

    ICalculator

---
+++ small_calculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace small_calculator
+{
+    class Calculator : ICalculator
+    {
+        public int Result { get; }
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subtract(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Multiply(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Divide(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modulus(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ small_calculator/ICalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace small_calculator
+{
+    interface ICalculator
+    {
+       
+            int Result { get; } // property keeping the current result of the calculator
+            void Reset(); // resets the current result of the calculator
+            void Add(int x); // adds x to the current result
+            void Subtract(int x); // subtracts x from the current result
+            void Multiply(int x); // multiplies the current result with x
+            void Divide(int x); // divides the currents result with x (integer division)
+            void Modulus(int x); // divides the current result with x and stores the remainder as
+            // the current result 
+        
+    }
+}
